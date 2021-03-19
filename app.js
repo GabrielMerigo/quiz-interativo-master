5d991ca23c9cbbd2e7e5c6022212893987bf553b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-03-18 23:57:12 -0300

    Quiz Feito 0k

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const form = document.querySelector('.quiz-form')
-const paragraphSuccess = document.querySelector('.paragraphSuccess')
+const paragraphSuccess = document.querySelector('.paragraphSuccessContainer')
 const correctAnswers = ['A', 'C', 'C', 'B']
 
 let score = 0
 
 const getUserAnswers = () => {
-  const userAnswers = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value
-  ];
+  let userAnswers = []
+
+  correctAnswers.forEach((_, index) => {
+    const userAnswer = form[`inputQuestion${index + 1}`].value;
+    userAnswers.push(userAnswer)
+  })
+
+  console.log(userAnswers);
 
   return userAnswers
 }
@@ -22,29 +24,28 @@ const calculateUserScore = (userAnswers) =>
     }
   })
 
+const animateFinalScore = () => {
+  let counter = 0;
+  const timer = setInterval(() => {
+    if (counter < score) {
+      ++counter
+      paragraphSuccess.innerHTML = `<strong>Parabéns, você acertou ${score}%</strong>`
+      clearInterval(timer)
+    }
+  }, 10)
+}
+
 form.addEventListener('submit', event => {
   score = 0
   event.preventDefault()
-
-  // pega as respostas do usuário
   const userAnswers = getUserAnswers()
 
-  // Calcula pontuação do user
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
   calculateUserScore(userAnswers)
-
-  let counter = 0;
-
-  // conta o score do usuário
-  const timer = setInterval(() => {
-    if (counter < score) {
-      counter++
-      clearInterval(timer)
-    }
-  }, 3000)
-
-  scrollTo(0, 0)
-
-  // seta o % do usuário na tela.
-  paragraphSuccess.innerHTML = `<strong>Parabéns, você acertou ${score}%</strong>`
+  animateFinalScore()
 })
 
