d6bbed47f8046fd275425c2dfb7f5f6016655eba
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-03-18 22:19:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,25 +2,40 @@ const form = document.querySelector('.quiz-form')
 const paragraphSuccess = document.querySelector('.paragraphSuccess')
 const correctAnswers = ['A', 'C', 'C', 'B']
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
+let score = 0
 
-  let score = 0
+const getUserAnswers = () => {
   const userAnswers = [
     form.inputQuestion1.value,
     form.inputQuestion2.value,
     form.inputQuestion3.value,
     form.inputQuestion4.value
-  ]
+  ];
 
+  return userAnswers
+}
+
+const calculateUserScore = (userAnswers) =>
   userAnswers.forEach((userAnswer, index) => {
     if (userAnswer === correctAnswers[index]) {
       score += 25
     }
   })
 
+form.addEventListener('submit', event => {
+  score = 0
+  event.preventDefault()
+
+  // pega as respostas do usuário
+
+  const userAnswers = getUserAnswers()
+
+  // Calcula pontuação do user
+  calculateUserScore(userAnswers)
+
   let counter = 0;
 
+  // conta o score do usuário
   const timer = setInterval(() => {
     if (counter < score) {
       counter++
@@ -30,6 +45,7 @@ form.addEventListener('submit', event => {
 
   scrollTo(0, 0)
 
+  // seta o % do usuário na tela.
   paragraphSuccess.innerHTML = `<strong>Parabéns, você acertou ${score}%</strong>`
 })
 
